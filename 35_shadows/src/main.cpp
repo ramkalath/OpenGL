e4c4f177e622915f2d86807c5fb11d51c482ba4e
ramkalath <ramkalath@gmail.com>
2019-10-01 13:46:14 +0530

    have to do shadows

---
+++ 35_shadows/src/main.cpp
@@ -0,0 +1,83 @@
+/*****************************************************************************
+* Filename : main.cpp
+* Date : 30-9-2019
+* Author : Ram
+* Email : ramkalath@gmail.com
+* Breif Description : Shadows
+* Detailed Description : 1) step 1 is to generate a directional light over two plates. One hovering over the other.
+*****************************************************************************/
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <SOIL/SOIL.h>
+#include "../include/shader.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <math.h>
+#include <vector>
+
+using namespace std;
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	// When the user presses the escape key, we set the window should close property to true, and close the application.
+	if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int width = 640, height = 800;
+
+int main()
+{
+	// glfw stuff ====================================================================
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "specular_lighting", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    if(window == nullptr)
+    {
+    	std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+    }
+	glewExperimental = GL_TRUE;
+
+	if(glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	glEnable(GL_DEPTH_TEST);
+
+
+	//TODO(ram): load 4 models here. Just like buckys
+	// 1) 
+
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+        glClearColor(0.27f, 0.27f, 0.27f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		
+
+		glfwSwapBuffers(window);
+	}
+	glfwTerminate();
+	return 0;
+}
