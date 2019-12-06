7678991455d5c0f8542b74bb8a5824a50bde247b
ramkalath <ramkalath@gmail.com>
2019-12-06 15:54:43 +0530

    some crappy commit

--- 30_Geometry_Shader/GeometryShader_explosion/build/CMakeFiles/assimp_0.dir/cmake_clean.cmake
+++ 30_Geometry_Shader/GeometryShader_explosion/build/CMakeFiles/assimp_0.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
+  "../bin/assimp_0"
+  "../bin/assimp_0.pdb"
   "CMakeFiles/assimp_0.dir/include/gamesettings.cpp.o"
   "CMakeFiles/assimp_0.dir/include/modelloader.cpp.o"
   "CMakeFiles/assimp_0.dir/include/shader.cpp.o"
   "CMakeFiles/assimp_0.dir/src/main.cpp.o"
-  "../bin/assimp_0.pdb"
-  "../bin/assimp_0"
 )
 
 # Per-language clean rules from dependency scanning.
